fd5005e9574f51fbfa02732177ac055b06fbc13a
Mariyq1 <m4697597@gmail.com>
2023-01-02 17:34:25 +0100

    Create file and drop components

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div className="Header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8uYCpl_KfOiL3rP9hrDgpMUBywTUkiJebEA&usqp=CAU"
+        alt="img"/>
+        </div>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <div className="Navbar">
+            <ul>
+          <li><a>Profile</a></li>
+          <li><a>Messages</a></li>
+          <li><a>News</a></li>
+          <li><a>Music</a></li>
+          <li><a>Settings</a></li>
+          
+        </ul>
+        </div>
+    )
+}
