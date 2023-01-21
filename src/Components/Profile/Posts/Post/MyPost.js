1ff04a69064a01c36a5b6c7211fb1479acc16725
Mariyq1 <m4697597@gmail.com>
2023-01-21 04:24:00 +0100

    Delete img

--- src/Components/Profile/Posts/Post/MyPost.js
+++ src/Components/Profile/Posts/Post/MyPost.js
@@ -5,8 +5,7 @@ export default function MyPost(props){
     return <div className="MyPost">
         <div className={classes.item}>
             Post 1
-            <img src="http://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2022-06/20130124_gaf_ux5_110.jpg?itok=MRhdFlUi"
-            alt="ava"/>
+            
              {props.name}
              <span>Likes</span>{props.counts}
         </div>
