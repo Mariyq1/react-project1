876f8ff4894619da0b5a0043bc1a5af23c2ae4ec
Mariyq1 <m4697597@gmail.com>
2023-01-26 19:34:26 +0100

    Remove axios inside componentDidMount

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 import userPhoto from "../../Images/userPhoto.png"
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(
+    
+    componentDidMount(){
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(
             response=>{
                 this.props.setUsers(response.data.items)
             }
