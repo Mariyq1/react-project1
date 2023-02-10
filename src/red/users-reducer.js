252e918b5128baf16506fea5871ad6344a0b3dbd
Mariyq1 <m4697597@gmail.com>
2023-02-10 19:55:31 +0100

    Create selectors for usersContainer

--- src/red/users-reducer.js
+++ src/red/users-reducer.js
@@ -72,10 +72,11 @@ export const setTotalUsersCount= (totalUsersCount) => ({type: SET_TOTAL_USERS_CO
 export const toggleIsFetching = (isFetching) =>({type: TOGGLE_IS_FETCHING, count:isFetching})
 export const toggleFollowingProgress = (isFetching, userId) =>({type: TOGGLE_IS_FOLLOWING_PROGRESS, count:isFetching, userId})
 
-export const getUsers = (currentPage, pageSize) =>{
+export const requestUsers = (page, pageSize) =>{
     return (dispatch) => {
     dispatch(toggleIsFetching(true));
-                usersAPI.getUsers(currentPage, pageSize).then(data=>{
+    dispatch(setCurrentPage(page));
+                usersAPI.getUsers(page, pageSize).then(data=>{
                 dispatch(setUsers(data.items));
                 dispatch(setTotalUsersCount(data.totalCount));
                 dispatch(toggleIsFetching(false));
