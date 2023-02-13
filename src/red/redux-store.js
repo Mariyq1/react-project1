5c2dd97d018b2595d8bbc8db71239127c3099a61
Mariyq1 <m4697597@gmail.com>
2023-02-13 19:56:14 +0100

    Add redux dev tools

--- src/red/redux-store.js
+++ src/red/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore, compose } from "redux";
 import profileReducer from "./profile-reducer";
 import messagesReducer from "./messages-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -17,9 +17,9 @@ app: appReducer
 }
 
 );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = legacy_createStore(reducers,composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-
-let store = legacy_createStore(reducers, applyMiddleware(thunkMiddleware));
-window.store = store;
+window.__store__ = store;
 
 export default store;
